fab43cbdc6c1a9d808f854de61e5866177e23074
naomiwashere <116879380+naomiwashere@users.noreply.github.com>
2023-01-14 00:18:49 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ const {
    indonesia,
    english,
    spanyol
-} = require(`./language`)
+} = require(`./english`)
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
@@ -13,7 +13,7 @@ global.APIs = {
 
 // Free apikey (Apikey expired silahkan login terus ganti V12 dgn apikey lu)
 global.APIKeys = {
-   'https://api.zeeoneofc.my.id': 'V12', // 👉 login https://api.zeeoneofc.xyz to get apikey
+   'https://api.zeeoneofc.my.id': 'rj27HMdF', // 👉 login https://api.zeeoneofc.xyz to get apikey
 }
 
 // setting 
@@ -21,34 +21,34 @@ global.autoread = false // auto read message
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
-global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
+global.autoblok212 = true //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
-global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.botname = "naomi cantik banget" //namabot kalian
+global.ownername = "naomi comel" //nama kalian
+global.myweb = "https://s.id/naomisicantik" //bebas asal jan hapus
+global.youtube = "https://youtube.com/@naomi" //bebas asal jan hapus
+global.myweb2 = "https://s.id/naomisicantik" //bebas
+global.email = "nothingtoseehere24242@gmail.com" //bebas
+global.region = "Brunei" //bebas
+global.timezone = 'Asia/Brunei' //  timezone wib
+global.premium = ['60109018435'] //premium user
+global.owner = ["60109018435", "60109018435"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "60109018435" // nomor wa kalian
+global.ownernomerr = "+60109018435" //nmr wa kalian
+global.thumbnail = "./CUTE GIRL LA.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./CUTE GIRL LA.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = 'naomi cantik banget' //sticker wm ubah
+global.author = 'by naomi cantik banget' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
@@ -122,16 +122,16 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['', '!', '.', '🐦', '🐤', '🗿']
 global.sp = '⭔'
 global.mess = {
-   sukses: '🤗Done, Oke Desu~',
-   admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
-   botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-   owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
-   group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
-   private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
-   bot: 'Fitur Khusus Pengguna Nomor Bot',
+   sukses: 'yay success',
+   admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin blee !',
+   botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin blee !',
+   owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner blee !',
+   group: 'Perintah Ini Hanya Bisa Digunakan Di Group blee !',
+   private: 'Perintah Ini Hanya Bisa Digunakan Di Private blee !',
+   bot: 'only fitur bot',
    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
-   wait: '⏳ Sedang Di Proses',
-   lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+   wait: 'sabar ye sayangku',
+   lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner blee !',
    example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
